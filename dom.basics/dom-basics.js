e02aca45d7b5716eeb7bf970e658c6e0a95a8599
TaborVav <taborvavra@byui.edu>
2025-01-18 19:59:34 -0700

    Mission Statement 2

--- dom.basics/dom-basics.js
+++ dom.basics/dom-basics.js
@@ -10,3 +10,12 @@ document.body.appendChild(image);
 const newDiv = document.createElement("div");
 newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
+
+const newSection = document.createElement("Section")
+const newH2 = document.createElement("h2")
+newH2.innerText = "DOM Basics";
+newSection.appendChild(newH2);
+const newP = document.createElement("p")
+newP.innerText = "This was added through Javascript";
+newSection.appendChild(newP);
+document.body.appendChild(newSection);
